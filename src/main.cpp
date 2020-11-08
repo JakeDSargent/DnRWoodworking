a8936ac3aa2558c7e712707fc0d8232bec8ebfcd
= <=>
2020-11-08 11:40:52 -0500

    no JSON, CSV instead

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,7 @@
 #include <iostream>
 #include <string>
 #include <fstream>
-#include "json.hpp"
 #define DAVID "Hello David!\n"
-
 
 class Customer {
 public:
@@ -11,6 +9,11 @@ public:
     std::string address;
     int value_cents;
     std::string description;
+
+    Customer(std::string in_name, std::string in_address) {
+      set_name(in_name);
+      address = in_address;
+    }
 
     void set_name(std::string new_name) {
         name = new_name;
@@ -21,11 +24,15 @@ public:
     std::string get_name_2() {
         return name;
     }
+    std::string get_csv() {
+      std::string csv_line = "";
+      csv_line += name + "," + address + ",";
+      return csv_line;
+    }
 };
 
 int main() {
-    Customer c1 = Customer();
-    c1.set_name("Jake");
-    std::cout << c1.get_name_2();
+    Customer c1 = Customer("Jake", "Worcester, MA");
+    std::cout << c1.get_csv();
 
 }
