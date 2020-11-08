19fb7c5234e625a445df2cd3ea204c2d34c238bb
Jake Sargent <jds.m19productions@gmail.com>
2020-11-08 15:54:00 +0000

    not gonna use JSON, CSV instead

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <fstream>
 #define DAVID "Hello David!\n"
 
 class Customer {
@@ -18,11 +19,17 @@ public:
     std::string get_name_2() {
         return name;
     }
+    std::string get_csv() {
+      std::string csv_line = "";
+      csv_line += name + "," + address + ",";
+      return csv_line;
+    }
 };
 
 int main() {
     Customer c1 = Customer();
     c1.set_name("Jake");
-    std::cout << c1.get_name_2();
+    c1.address = "Worcester, MA";
+    std::cout << c1.get_csv();
 
 }
